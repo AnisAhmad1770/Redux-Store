7091211f89d1e6a774f1da6cc8abf4430aabb762
Anis Ahmad <anisahmad1770@gmail.com>
2023-08-01 17:25:02 +0500

    store12

---
+++ src/components/Product.js
@@ -0,0 +1,46 @@
+import { useState,useEffect } from "react"
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+const Product = () => {
+    const[products,getProducts]=useState([]);  
+    
+    useEffect(()=>{
+        //api
+        fetch('https://fakestoreapi.com/products')
+        .then(data=>data.json())
+        .then(result=> getProducts(result))
+        }
+        ,[]);
+        
+  const cards =products.map(product=>(
+    <div className="col-md-3 " style={{marginBottom:'10px'}}>
+    
+    <Card key={product.id} className="h-100">
+      <div className="text-center">
+        <Card.Img variant="top" src={product.image} style={{width:'100px',height:'130px'}} />
+      </div>
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>
+         PKR. {product.price}
+        </Card.Text>
+        
+      </Card.Body>
+      <Card.Footer style={{backgroundColor:'white'}}>
+      <Button variant="primary">Add to Cart</Button>
+      </Card.Footer>
+    </Card>
+    </div>
+    
+  ))
+  return (
+    <>
+     <h1>Product Dashboard</h1> 
+     <div className="row">
+    {cards}
+    </div>
+    </>
+  )
+}
+
+export default Product
---
+++ src/components/RootLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBarPanel from './NavBarPanel'
+const RootLayout = () => {
+  return (
+    <div>
+      <NavBarPanel/>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  )
+}
+
+export default RootLayout
